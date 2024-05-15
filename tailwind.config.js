c4e9f2c5f69f5355179a8d686667e218644b9583
Garth Murray <garthmurray91@gmail.com>
2024-05-15 15:26:04 -0400

    Added starters for additional pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { default: daisyui } = require("daisyui");
+
 module.exports = {
 	content: ["./**/*.html"],
 	theme: {
@@ -7,6 +9,42 @@ module.exports = {
 		extend: {
 			colors: {},
 		},
+	},
+	daisyui: {
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+			"dim",
+			"nord",
+			"sunset",
+		],
 	},
 	variants: {},
 	plugins: [
